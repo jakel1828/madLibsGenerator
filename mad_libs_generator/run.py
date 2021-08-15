416a6aff522de87c91c9fbd5580355b4929931a2
Jake LaCaze <github@jakleacaze.com>
2021-08-14 19:00:31 -0500

    consolidated prompts and messages

--- mad_libs_generator/run.py
+++ mad_libs_generator/run.py
@@ -1,51 +1,40 @@
-print('Enter an adjective.')
-adj1 = input()
-print('Enter an adjective.')
-adj2 = input()
-print('Enter a noun.')
-noun1 = input()
-print('Enter a noun.')
-noun2 = input()
-print('Enter a plural noun.')
-pnoun1 = input()
-print('Enter a game.')
-game1 = input()
-print('Enter a plural noun.')
-pnoun2 = input()
-print('Enter a verb ending in "ing".')
-verbing1 = input()
-print('Enter a verb ending in "ing".')
-verbing2 = input()
-print('Enter a plural noun.')
-pnoun3 = input()
-print('Enter a verb ending in "ing".')
-verbing3 = input()
-print('Enter a noun.')
-noun3 = input()
-print('Enter a plant.')
-plant1 = input()
-print('Enter a part of the body.')
-bodypart1 = input()
-print('Enter a place.')
-place1 = input()
-print('Enter a verb ending in "ing".')
-verbing4 = input()
-print('Enter an adjective.')
-adj3 = input()
-print('Enter an number.')
-number1 = input()
-print('Enter a plural noun.')
-pnoun4 = input()
+''' start prompts '''
+
+adj1 = input('Enter an adjective: ')
+adj2 = input('Enter an adjective: ')
+noun1 = input('Enter a noun: ')
+noun2 = input('Enter a noun: ')
+pnoun1 = input('Enter a plural noun: ')
+game1 = input('Enter a game: ')
+pnoun2 = input('Enter a plural noun: ')
+verbing1 = input('Enter a verb ending in "ing": ')
+verbing2 = input('Enter a verb ending in "ing": ')
+pnoun3 = input('Enter a plural noun: ')
+verbing3 = input('Enter a verb ending in "ing": ')
+noun3 = input('Enter a noun: ')
+plant1 = input('Enter a plant: ')
+bodypart1 = input('Enter a part of the body: ')
+place1 = input('Enter a place: ')
+verbing4 = input('Enter a verb ending in "ing": ')
+adj3 = input('Enter an adjective: ')
+
+''''''
+
+number1 = input('Enter a whole number: ')
+pnoun4 = input('Enter a plural noun: ')
+
+''' Let users know they've entered all prompts.'''
+
 print('You have entered all the prompts. Now for the mad lib!')
+
+''' Print complete mad lib. '''
+
 print('A vacation is when you take a trip to some ' + adj1 + ' place with your ' + adj2 + ' family. Usually you go to'
-       ' some place that is near a/an ' + noun1 + ' or up on a/an ' + noun2 + '. A good vacation place is one where'
-       ' you can ride ' + pnoun1 + ' or play ' + game1 + ' or go hunting for ' + pnoun2 + '. I like to spend my time '
-        + verbing1 + ' or ' + verbing2 + '. When parents go on a vacation, they spend their time eating three ' + pnoun3
-        + ' a day, and fathers play golf, and mothers sit around ' + verbing3 + '. Last summer, my little brother fell in' \
-        ' a/an ' + noun3 + ' and got poison ' + plant1 + ' all over his ' + bodypart1 + '. My family is going to (the)'
-        + place1 + ', and I will practice ' + verbing4 + '. Parents need vacations more than kids because parents are'
-        ' always very ' + adj3 + ' and because they have to work ' + number1 + ' hours every day all year making enough '
-        + pnoun4 + ' to pay for the vacation.')
-
-
-
+' some place that is near a/an ' + noun1 + ' or up on a/an ' + noun2 + '. A good vacation place is one where'
+' you can ride ' + pnoun1 + ' or play ' + game1 + ' or go hunting for ' + pnoun2 + '. I like to spend my time '
++ verbing1 + ' or ' + verbing2 + '. When parents go on a vacation, they spend their time eating three ' + pnoun3
++ ' a day, and fathers play golf, and mothers sit around ' + verbing3 + '. Last summer, my little brother fell in' \
+' a/an ' + noun3 + ' and got poison ' + plant1 + ' all over his ' + bodypart1 + '. My family is going to (the)'
++ place1 + ', and I will practice ' + verbing4 + '. Parents need vacations more than kids because parents are'
+' always very ' + adj3 + ' and because they have to work ' + number1 + ' hours every day all year making enough '
++ pnoun4 + ' to pay for the vacation.')
